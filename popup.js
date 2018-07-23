f4e535b6f15dab83ea9615a1b5fd3c6222f4c3df
Maria Turner <maturner@seas.upenn.edu>
2018-07-23 12:08:05 -0400

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,8 +1,6 @@
 $(function () {
 
-  // Event listening code goes here.
-  // Be sure to check popup.html to learn the `id` attributes of the apply/remove filter
-  // buttons and the username input box - you'll need those to listen for events!
+  // Event listening
   $('#filter-off').click(function () {
     chrome.tabs.executeScript({ code: 'window.parser.filter(false)' });
   });
@@ -15,8 +13,5 @@ $(function () {
     var user = $('#username').val();
     chrome.tabs.executeScript({ code: 'window.parser.parse(' + user + ')' });
   });
-
-  // HINT: You can't access window.parser here. You'll have to use chrome.tabs.executeScript
-  // to call window.parser.filter and window.parser.parse.
 
 });
