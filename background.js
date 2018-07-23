4b72b5b8313d17bfc65b04ea3ea70b3fb016b308
Maria Turner <maturner@seas.upenn.edu>
2018-07-23 12:06:18 -0400

    Update background.js

--- background.js
+++ background.js
@@ -1,10 +1,10 @@
-// when the extension is installed...
+
 chrome.runtime.onInstalled.addListener(function () {
 
-  // replace all existing rules...
+
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
 
-    // with these new rules:
+
     chrome.declarativeContent.onPageChanged.addRules([
       {
         conditions: [
