91b4e70877be0d13392629252a46d9156fcccbfe
Maria Turner <maturner@seas.upenn.edu>
2018-07-23 12:07:17 -0400

    Update comment-parser.js

--- comment-parser.js
+++ comment-parser.js
@@ -1,15 +1,10 @@
 var CommentParser = function ($comments, op) {
-  // Save the $comments and op as state!
+  // Save the $comments and op as state
   this.$comments = $comments;
   this.op = op;
 };
 
 
-// parse and filter implementations go here.
-//parse should take a String username as a parameter
-//apply the classes answered-chain and unanswered-chain to each top level comment
-//based on whether username shows up in the chain
-//only one of these classes should ever be applied to a top level comment at a time
 CommentParser.prototype.parse = function (username) {
 //stuff
 for(var i = 0; i < this.$comments.length; i++) {
@@ -51,10 +46,4 @@ for (var i = 0; i < this.$comments.length; i++) {
       $(this.$comments[i]).css('display', 'block');
     }
   }
-  //when page first loads, filter(true) should be called in the parse method & 
-  //unanswered chains should be hidden
-  //if filter(false) is subsequently called unanswered chains should be shown
-  //don't change classes; use $topLevelComment.css('display', 'block') to show comments
-  //$topLevelComment.css('display', 'none'); to hide comments
-
 };
